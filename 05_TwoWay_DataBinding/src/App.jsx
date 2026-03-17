a0b520a12f9768246b523ba938b72b8a4b25dd0a
shaheed-khan <kshaheed123@gmail.com>
2026-03-18 02:49:06 +0530

    Using Two Way Binding for handling form data

---
+++ 05_TwoWay_DataBinding/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+const App = () => {
+  const [title, setTitle] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Form submitted by',title)
+    setTitle('')
+  }
+  
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder='Enter your name' 
+          value={title}
+          onChange={(e) => 
+            setTitle(e.target.value)
+          }
+        /> <br />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  )
+}
+
+
+
+export default App
