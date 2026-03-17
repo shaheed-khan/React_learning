462e68392b795f198fd2957a1bbc40ed44036e75
shaheed-khan <kshaheed123@gmail.com>
2026-03-18 01:35:07 +0530

    using array of abject with useState and using batch update method to update age

--- 02_hooks/src/App.jsx
+++ 02_hooks/src/App.jsx
@@ -1,18 +1,18 @@
 
 import { useState } from 'react'
 const App = () => {
-  const [num, setNum] = useState([10,20,30,40])
+  const [num, setNum] = useState({user:"sarthak", age: 20})
   const handleClick = () => {
-    const newNum =[num]
-    newNum.push(50)
-    setNum(newNum)    
+    setNum(prev=>({...prev, age: 21}))    
   }
   return (
     <div>
-      <h1> THE Value of num is: {num}</h1>
+      <h1> Name: {num.user} <br /> Age: {num.age}</h1>
       <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
+
+
 
 export default App
