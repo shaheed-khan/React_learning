807304b4364ba3b698489c339edd587f26355903
shaheed-khan <kshaheed123@gmail.com>
2026-03-18 01:18:23 +0530

    updated the css file and understanding useState with array

--- 02_hooks/src/App.jsx
+++ 02_hooks/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+
 import { useState } from 'react'
 const App = () => {
-  const [num, setNum] = useState(0)
+  const [num, setNum] = useState([10,20,30,40])
   const handleClick = () => {
-    setNum(num + 1)
+    const newNum =[num]
+    newNum.push(50)
+    setNum(newNum)    
   }
   return (
     <div>
-      <h1> THE Value of num is{num}</h1>
+      <h1> THE Value of num is: {num}</h1>
       <button onClick={handleClick}>Click me</button>
     </div>
   )
