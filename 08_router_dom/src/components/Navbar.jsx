e2d74e4a78f0b3e7676ca2764b8eb344e2320deb
shaheed-khan <kshaheed123@gmail.com>
2026-03-28 17:23:00 +0530

    understanding  basic router dom

---
+++ 08_router_dom/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+import {Link} from 'react-router-dom'
+const Navbar = () => {
+  return (
+     <div className='nav'>
+       <h2>MASKAN</h2>
+        <div>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
+        </div>
+     </div>
+  )
+}
+
+export default Navbar
---
+++ 08_router_dom/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <h1>Contact page</h1>
+    
+    </div>
+  )
+}
+
+export default contact
